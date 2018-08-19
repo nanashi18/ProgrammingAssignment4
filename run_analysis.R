412c0b682c6b4308a30415af38f175ed2c8fbc1b
Teng Zhang <tengz@seas.upenn.edu>
2018-08-18 21:23:13 -0400

    first push

---
+++ run_analysis.R
@@ -0,0 +1,60 @@
+#read features
+features <- read.table("./UCI HAR Dataset/features.txt", sep= " ",
+                      col.names=c("featureid","featurename"))
+#read activity labels
+activitylabels <- read.table("./UCI HAR Dataset/activity_labels.txt", sep = " ",
+                             col.names = c("activityid","activityname"))
+
+#get activity name
+getActivityName <- function(x) {
+  as.character(activitylabels[x,2])
+}
+
+#read subjects in test data
+testsubjects <- unlist(read.table("./UCI HAR Dataset/test/subject_test.txt"),use.names=FALSE)
+#read Xs in test data
+testX <- read.table("./UCI HAR Dataset/test/X_test.txt")
+#set column names of testX to feature names
+colnames(testX) <- as.character(features[[2]])
+#read Ys in test data
+testY <- read.table("./UCI HAR Dataset/test/Y_test.txt")
+#map from activity id to name
+testYNamed <- unlist(lapply(testY, getActivityName), use.names=FALSE)
+
+
+#read subjects in train data
+trainsubjects <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+#read X in train data
+trainX <- read.table("./UCI HAR Dataset/train/X_train.txt")
+#set column names of testX to feature names
+colnames(trainX) <- as.character(features[[2]])
+#read Y in train data
+trainY <- read.table("./UCI HAR Dataset/train/Y_train.txt")
+#map from activity id to name
+trainYNamed <- unlist(lapply(trainY, getActivityName), use.names=FALSE)
+
+#Merges the training and the test sets to create one data set
+
+#bind rows of train X and test X
+bindTestTrainX <- rbind(trainX,testX)
+#filter columns
+#only columns with "mean()" or "std()" are remained
+bindTestTrainX <- bindTestTrainX[grepl("mean\\(\\)|std\\(\\)", names(bindTestTrainX))]
+
+#bind rows of train Y and test Y
+bindTestTrainY <- append(trainYNamed,testYNamed)
+bindTestTrainSubjects <- append(unlist(trainsubjects, use.names=FALSE)
+                                ,unlist(testsubjects, use.names=FALSE))
+
+#bindAllTest is the data set for step 4 
+bindAllTest <- cbind(bindTestTrainSubjects,bindTestTrainY,bindTestTrainX)
+colnames(bindAllTest)[1] <- "subjectid"
+colnames(bindAllTest)[2] <- "activityname"
+
+#tidy data for step 5
+tidydata <- aggregate(bindAllTest[,seq(3,length(names(bindAllTest)))], list(bindAllTest$subjectid,bindAllTest$activityname),mean)
+colnames(tidydata)[1] <- "subjectid"
+colnames(tidydata)[2] <- "activityname"
+write.table(tidydata, file = "./UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
+
+
